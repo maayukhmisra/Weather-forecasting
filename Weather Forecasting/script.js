2b2b049c5a01c5f09d9412f67363a060616c77b9
Maayukh Misra <maayukhmisra@gmail.com>
2026-01-09 22:19:19 +0530

    Add files via upload

---
+++ Weather Forecasting/script.js
@@ -0,0 +1,32 @@
+//const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+const apiKey = "963662958dff83bce60a90cf940d2aa3"; 
+
+const inputvalue = document.querySelector(".input");
+const windspeed = document.querySelector(".windspeed");
+const desc = document.querySelector(".desc");
+const temp = document.querySelector(".temp");
+
+function input(e) {
+
+    e.preventDefault();
+    const city = document.querySelector("#inputvalue").value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+
+            var descp = data.weather[0].description;
+            var tempt = data.main.temp;
+            var windspd = data.wind.speed;
+
+            windspeed.innerHTML = "Windspeed: " + windspd;
+            desc.innerHTML = "Desc: " + descp;
+            temp.innerHTML = "temp: " + tempt;
+        });
+}
+
+inputvalue.addEventListener("submit", input);
